6fc6819f8d64d909c8d5bf9d8bf4c55df2fd24f2
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2018-04-09 19:35:20 +0800

    编写文章 | 初探HTTP缓存之谜

--- XXX未完成/浏览器缓存之谜/server.js
+++ XXX未完成/浏览器缓存之谜/server.js
@@ -1,7 +1,7 @@
 const http = require("http")
 
 let server = http.createServer(function(request, response){
-    response.setHeader("Cache-Control", "max-age=3600")
+    response.setHeader("Expires", new Date(Date.now() + 10000).toDateString())
     response.write("223")
     response.end()
 })
