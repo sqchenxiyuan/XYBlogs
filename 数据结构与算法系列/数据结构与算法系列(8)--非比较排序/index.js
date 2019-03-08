e972fe4f962c3ecb4348f0d70c43a02536a0cc98
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2019-03-08 14:41:15 +0800

    完成文章 | 数据结构与算法系列(8)--非比较排序

--- 数据结构与算法系列/数据结构与算法系列(8)--非比较排序/index.js
+++ 数据结构与算法系列/数据结构与算法系列(8)--非比较排序/index.js
@@ -7,6 +7,7 @@ console.log(arr.join(","))
 console.log(Array.from(arr).sort((a, b) => a - b).join(","))
 console.log(CountSort(arr).join(","))
 console.log(BaseSort(arr).join(","))
+console.log(BucketSort(arr, 10).join(","))
 
 function CountSort(arr){
     let min = Infinity
@@ -62,6 +63,44 @@ function BaseSort(arr){
 
         x = x * 10
         if (x > max) break
+    }
+
+    return arr
+}
+
+function BucketSort(arr, bukectSize = 10){
+    let min = Infinity
+    let max = 0
+    let len = arr.length
+    for (let i = 0; i < len; i++){
+        if (arr[i] < min) min = arr[i]
+        if (arr[i] > max) max = arr[i]
+    }
+
+    let bucketNum = parseInt((max - min) / bukectSize) + 1
+    let buckets = new Array(bucketNum).fill(0).map(_ => new Array())
+
+    for (let i = 0; i < len; i++){
+        let b = parseInt((arr[i] - min) / bukectSize)
+        let bucket = buckets[b]
+        let len = bucket.length
+        bucket[len] = arr[i]
+        for (let j = len - 1; j >= 0; j--){
+            if (bucket[j] < arr[i]){
+                bucket[j + 1] = bucket[j]
+            } else {
+                bucket[j + 1] = arr[i]
+                break
+            }
+        }
+    }
+
+    let p = 0
+    for (let i = 0; i < buckets.length; i++){
+        for (let j = 0; j < buckets[i].length; j++){
+            arr[p] = buckets[i][j]
+            p++
+        }
     }
 
     return arr
